c09cfbb077917dc80539ee781d4c7d6a90ac0cc9
Enrico Bianchi <enrico.bianchi@ymail.com>
2012-08-30 12:36:45 +0200

    Server: Improved command execution

--- server/src/org/memento/server/management/Operation.java
+++ server/src/org/memento/server/management/Operation.java
@@ -3,11 +3,12 @@
  Project       Memento
  Description   A backup system
  License       GPL version 2 (see GPL.txt for details)
-*/
-
+ */
 package org.memento.server.management;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import org.memento.server.storage.DbStorage;
 import org.memento.server.storage.FileStorage;
 
@@ -18,16 +19,34 @@ import org.memento.server.storage.FileStorage;
 public abstract class Operation implements Properties {
 
     public abstract void setDbStore(DbStorage dbstore);
+
     public abstract DbStorage getDbStore();
+
     public abstract void setFsStore(FileStorage fsstore);
+
     public abstract FileStorage getFsStore();
 
     public void execCommand(String command) throws IOException, InterruptedException {
+        BufferedReader bre;
         Process p;
-        
+        String line;
+        String retString;
+        int retCode;
+
         p = Runtime.getRuntime().exec(command);
-        p.wait();
+        retCode = p.waitFor();
+
+        if (retCode != 0) {
+            retString = "";
+            bre = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+            while ((line = bre.readLine()) != null) {
+                retString = retString + line + "\n";
+            }
+
+            throw new IOException(retString);
+        }
     }
-    
+
     public abstract void run();
 }
