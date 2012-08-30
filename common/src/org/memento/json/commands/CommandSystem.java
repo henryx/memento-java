af5ea51f542876dcf86cec308c8e0b118fcfdc6a
Enrico Bianchi <enrico.bianchi@ymail.com>
2012-08-30 18:05:16 +0200

    Common: Added system command bean generator

---
+++ common/src/org/memento/json/commands/CommandSystem.java
@@ -0,0 +1,31 @@
+/*
+ Copyright (C) 2011 Enrico Bianchi (enrico.bianchi@gmail.com)
+ Project       Memento
+ Description   A backup system
+ License       GPL version 2 (see GPL.txt for details)
+*/
+
+package org.memento.json.commands;
+
+/**
+ *
+ * @author enrico
+ */
+public class CommandSystem extends Command {
+    private String value;
+
+    /**
+     * @return the value
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+    
+}
