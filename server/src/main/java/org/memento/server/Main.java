46d04f2968de88e04e414c7e7b3f4dea7402a24c
Enrico Bianchi <enrico.bianchi@ymail.com>
2014-11-26 17:33:18 +0100

    Server: Fixed version number

--- server/src/main/java/org/memento/server/Main.java
+++ server/src/main/java/org/memento/server/Main.java
@@ -32,7 +32,7 @@ public class Main {
 
     private Options opts;
     private Wini cfg;
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "2.0";
     public static Logger logger = Logger.getLogger("Memento");
 
     public Main() {
